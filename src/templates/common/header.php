0bfc69dd886cd3e8d5728ed3790db770c9aa7abf
Daniel Basílio <dbasilio15@gmail.com>
2024-04-11 15:49:23 +0100

    visual adjustments to the search bar

--- src/templates/common/header.php
+++ src/templates/common/header.php
@@ -2,13 +2,15 @@
     <header>
         <div class="topbar">
             <img src="../images/logo.png" />
-            <form action="/search" method="get">
-                <input type="search" name="q" placeholder="Search...">
-            </form>
-            <button class="primary-btn">Iniciar Sessão</button>
-            <div class="cart-btn">
-                <span>0</span>
-                <img src="../images/shopping_cart.png" />
+            <div class="topbar-right-items">
+                <form action="/search" method="get">
+                    <input type="search" name="q" placeholder="Procure por vendedor, marca, produto...">
+                </form>
+                <button class="primary-btn">Iniciar Sessão</button>
+                <div class="cart-btn">
+                    <span>0</span>
+                    <img src="../images/shopping_cart.png" />
+                </div>
             </div>
         </div>
         <nav>
