7b8c173693c071ad928cbb207eba39752282eae6
Daniel Basílio <dbasilio15@gmail.com>
2024-04-11 13:44:29 +0100

    homepage layout styling

--- src/templates/home_page/hero.php
+++ src/templates/home_page/hero.php
@@ -1,12 +1,9 @@
 <?php function drawHero() { ?>
-    <section>
-        <div class="hero">
-            <img src="../images/home_page/hero_image.png">
-            <h1>USE or SELL</h1>
-            <p>ARMÁRIO A ABARROTAR?<br>
-               AQUI ENCONTRAS A SOLUÇÃO PERFEITA PARA O ESVAZIARES!</p>
-            <button class="primary-btn">Explorar</button>
-        </div>
-        <img src="../images/hero.png" />
+    <section class="hero">
+        <img src="../images/home_page/hero_image.png">
+        <p>ARMÁRIO A ABARROTAR?<br>
+        AQUI ENCONTRAS A SOLUÇÃO PERFEITA PARA O ESVAZIARES!</p>
+        <button class="primary-btn">VENDE AGORA!</button>
+        <h1>USE or SELL</h1>
     </section>
 <?php } ?>
