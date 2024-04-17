65e315a3ae28d9fd84d2ff3ed445f297cf7e1538
daunisiaj <daunisia.j@gmail.com>
2024-04-17 19:46:57 +0100

    Revert "Testing"
    This reverts commit c00b4cb82a32738f4bba90570bfe530ae6d21322.

--- src/templates/home_page/hero.php
+++ src/templates/home_page/hero.php
@@ -4,6 +4,6 @@
         <p>ARMÁRIO A ABARROTAR?<br>
         AQUI ENCONTRAS A SOLUÇÃO PERFEITA PARA O ESVAZIARES!</p>
         <button class="primary-btn">VENDE AGORA!</button>
-        <h1>USE or TRASH</h1>
+        <h1>USE or SELL</h1>
     </section>
 <?php } ?>
