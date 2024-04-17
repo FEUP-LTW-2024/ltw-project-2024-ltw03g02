7c20e17ab335aacb876fc8542dc2dc6d2daebb77
daunisiaj <daunisia.j@gmail.com>
2024-04-17 20:39:18 +0100

    costumer support first layout

---
+++ src/pages/support.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Costumer Support</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../styles/general_style.css">
+        <link rel="stylesheet" type="text/css" href="../styles/home_page.css">
+        <?php include_once('../templates/common/header.php'); ?>
+        <?php include_once('../templates/common/footer.php'); ?>
+
+    </head>
+    <body>
+        <?php drawHeader(); ?>
+
+
+        
+        <?php drawFooter(); ?>
+    </body>
+</html>
