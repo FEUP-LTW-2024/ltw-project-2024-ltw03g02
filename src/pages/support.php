3a133ac0653a11e49c6df6348dd2508865ae2404
daunisiaj <daunisia.j@gmail.com>
2024-04-22 12:49:46 +0100

    pagina de apoio ao cliente

--- src/pages/support.php
+++ src/pages/support.php
@@ -4,16 +4,15 @@
         <title>Costumer Support</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="../styles/general_style.css">
-        <link rel="stylesheet" type="text/css" href="../styles/home_page.css">
+        <link rel="stylesheet" type="text/css" href="../styles/support.css">
         <?php include_once('../templates/common/header.php'); ?>
         <?php include_once('../templates/common/footer.php'); ?>
+        <?php include_once('../templates/costumer_service/support.php'); ?>
 
     </head>
     <body>
         <?php drawHeader(); ?>
-
-
-        
+        <?php clientSupport(); ?>
         <?php drawFooter(); ?>
     </body>
 </html>
