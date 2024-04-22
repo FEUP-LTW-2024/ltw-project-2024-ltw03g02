f9ff7771c49681f1668e7e7f74e83e3a3e8e3484
daunisiaj <daunisia.j@gmail.com>
2024-04-22 21:51:27 +0100

    support page done, returns first layout

---
+++ src/templates/costumer_service/support.php
@@ -0,0 +1,11 @@
+<?php function clientSupport() { ?>
+    <section class="support">
+        <img src="../images/costumer_service/support.png">
+        <h1>Apoio ao cliente</h1>
+        <form action="/action_page.php">
+        <textarea id="support" name="client_support" rows="15" cols="80" placeholder="Descreve o teu problema detalhadamente..." class="textarea"></textarea>
+        <br>
+        <input type="submit" value="Enviar" class="primary-btn">
+        </form>
+    </section>
+<?php } ?>
