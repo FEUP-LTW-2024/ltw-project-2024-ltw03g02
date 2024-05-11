297f3262d9b9264218402de663acf515d5355141
daunisiaj <daunisia.j@gmail.com>
2024-05-11 21:02:21 +0100

    fixed path

--- src/pages/profile_page.php
+++ src/pages/profile_page.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" type="text/css" href="../styles/common_style.css">
         <link rel="stylesheet" type="text/css" href="../styles/profile_page.css">
         <?php include_once('../templates/common/header.php'); ?>
-        <?php include_once('../templates/profile_page.php'); ?>
+        <?php include_once('../templates/profile_page/profile_page.php'); ?>
         <?php include_once('../templates/common/footer.php'); ?>
 
     </head>
