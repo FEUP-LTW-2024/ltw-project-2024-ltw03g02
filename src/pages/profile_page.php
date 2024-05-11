0dfb2e72ef4f94da8be293b2baf11431fe1eb9fd
daunisiaj <daunisia.j@gmail.com>
2024-05-11 18:27:51 +0100

    profile page 'page'

---
+++ src/pages/profile_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Clothes Cachet - USE or SELL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../styles/common_style.css">
+        <link rel="stylesheet" type="text/css" href="../styles/profile.css">
+        <?php include_once('../templates/common/header.php'); ?>
+        <?php include_once('../templates/profile.php'); ?>
+        <?php include_once('../templates/common/footer.php'); ?>
+
+    </head>
+    <body>
+        <?php drawHeader(); ?>
+
+        <?php drawProfile(); ?>
+        
+        <?php drawFooter(); ?>
+    </body>
+</html>
