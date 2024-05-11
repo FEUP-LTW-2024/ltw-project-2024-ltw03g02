d7640be4031ca21dee058b4a8b85459ee120f1ff
daunisiaj <daunisia.j@gmail.com>
2024-05-11 18:59:04 +0100

    ignore

--- src/pages/profile_page.php
+++ src/pages/profile_page.php
@@ -4,9 +4,9 @@
         <title>Clothes Cachet - USE or SELL</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="../styles/common_style.css">
-        <link rel="stylesheet" type="text/css" href="../styles/profile.css">
+        <link rel="stylesheet" type="text/css" href="../styles/profile_page.css">
         <?php include_once('../templates/common/header.php'); ?>
-        <?php include_once('../templates/profile.php'); ?>
+        <?php include_once('../templates/profile_page.php'); ?>
         <?php include_once('../templates/common/footer.php'); ?>
 
     </head>
@@ -14,7 +14,7 @@
         <?php drawHeader(); ?>
 
         <?php drawProfile(); ?>
-        
+
         <?php drawFooter(); ?>
     </body>
 </html>
