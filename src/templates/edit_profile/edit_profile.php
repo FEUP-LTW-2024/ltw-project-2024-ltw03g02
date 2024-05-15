b18277fd3ecfa30fd3180500ef32c03490287e66
daunisiaj <daunisia.j@gmail.com>
2024-05-15 12:03:16 +0200

    autocomplete in edit profile

--- src/templates/edit_profile/edit_profile.php
+++ src/templates/edit_profile/edit_profile.php
@@ -1,4 +1,5 @@
-<?php function drawEditProfile() { ?>
+<?php
+ function drawEditProfile() { ?>
     <section id="edit-profile">
         <h2>Editar perfil</h2>
         <form id="edit-profile-form" action="../actions/action_edit_profile.php" method="post" enctype="multipart/form-data">
@@ -8,37 +9,37 @@
             </div>
             <div>
                 <label for="username">Novo username</label>
-                <input type="text" name="username" class="username-input" required>
+                <input type="text" name="username" class="username-input" value="<?php echo $_SESSION['username']; ?>"required>
             </div>            
 
             <div>
                 <label for="email">Novo email</label>
-                <input type="text" name="email" class="email-input" required>
+                <input type="text" name="email" class="email-input" value="<?php echo $_SESSION['email']; ?>"required>
             </div>
             
             <div>
                 <label for="pass">Nova password</label>
-                <input type="text" name="pass" class="pass-input" required>
+                <input type="password" name="pass" class="pass-input" required>
             </div>
 
             <div>
                 <label for="gender">Novo gênero</label>
-                <input type="text" name="gender" class="gender-input" required>
+                <input type="text" name="gender" class="gender-input" value="<?php echo $_SESSION['gender']; ?>"required>
             </div>
 
             <div>
                 <label for="address">Novo endereço</label>
-                <input type="text" name="address" class="address-input" required>
+                <input type="text" name="address" class="address-input" value="<?php echo $_SESSION['address']; ?>"required>
             </div>
 
             <div>
-                <label for="profile_image_link">Nova foto de perfil</label>
-                <input type="file" name="profile_image_link" class="profile_image_link-input" accept="image/*" required>
+                <label for="profile_image_link">Nova foto de perfil (link)</label>
+                <input type="text" name="profile_image_link" class="profile_image_link-input" value="<?php echo $_SESSION['photo']; ?>"required>
             </div>
 
             <div>
                 <label for="phoneNumber">Novo contacto</label>
-                <input type="text" name="phoneNumber" class="phoneNumber-input" required>
+                <input type="text" name="phoneNumber" class="phoneNumber-input" value="<?php echo $_SESSION['phoneNumber']; ?>"required>
             </div>
 
             <button class="primary-btn" type="submit">Salvar mudanças</button>
