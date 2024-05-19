a2c8dbd4eff1d8783bfb50070db91aacec37b274
Daniel Basílio <dbasilio15@gmail.com>
2024-05-19 19:57:06 +0100

    added some style to finished page

--- src/templates/cart_page/finished.php
+++ src/templates/cart_page/finished.php
@@ -1,7 +1,7 @@
 <?php function drawFinished() { ?>
     <div class="container">
         <h1>Compra concluída com sucesso!</h1>
-        <p>Obrigado pela sua compra. Os produtos comprados estão visíveis na sua página de perfil, na secção Itens Comprados.</p>
+        <p>Obrigado pela sua compra.</p>
         <a href="../../pages/home_page.php" class="go-home-btn"><button id="go-home">Voltar para a página inicial </button></a>
     </div>
 
