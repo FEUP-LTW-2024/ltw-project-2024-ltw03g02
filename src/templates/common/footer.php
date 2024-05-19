19ae2e9d07e9f9fcf9f3d1f70f916951897a23cf
Daniel Basílio <dbasilio15@gmail.com>
2024-05-19 14:51:00 +0100

    added a link to about us page in the remaining footer links

--- src/templates/common/footer.php
+++ src/templates/common/footer.php
@@ -3,8 +3,8 @@
         <ul>
             <li><a href="../../pages/returns.php">Devoluções</a></li>
             <li><a href="../../pages/support.php">Apoio ao Cliente</a></li>
-            <li><a href="">Termos e Serviços</a></li>
-            <li><a href="">Contactos</a></li>
+            <li><a href="about_us.php">Termos e Serviços</a></li>
+            <li><a href="about_us.php">Contactos</a></li>
         </ul>
         <p>&copy; 2024 Clothes Cachet</p>
     </footer>
